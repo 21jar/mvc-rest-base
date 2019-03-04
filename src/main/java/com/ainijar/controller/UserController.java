7e597e7f7795ae8c9ebe468db738e27fce6ba670
slt <slt680@163.com>
2019-03-04 14:51:09 +0800

    优化代码

--- src/main/java/com/ainijar/controller/UserController.java
+++ src/main/java/com/ainijar/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.ainijar.controller;
 
 import com.ainijar.common.config.Result;
+import com.ainijar.domain.User;
+import com.ainijar.service.IUserService;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
+    private IUserService iUserService;
+
     @GetMapping("/say1/{word}")
     @ApiOperation(value = "测试接口", notes = "测试接口")
     public Result say1(@ApiParam(name = "word", value = "helloworld", required = true) @PathVariable String word) {
+        EntityWrapper ew = new EntityWrapper();
+        ew.where("name = {0}","");
+        User user = iUserService.selectOne(ew);
         return Result.success(word, "成功");
     }
 
